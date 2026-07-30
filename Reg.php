039e012019a62fb262266efc3350c022d7e68fab
martirosyanv192-svg <martirosyanv192@gmail.com>
2026-07-30 09:59:03 -0700

    Fix login and register for Railway

--- Reg.php
+++ Reg.php
@@ -274,8 +274,8 @@ session_start();
         <button type="submit" class="btn-glow">ՄՈՒՏՔ ԳՈՐԾԵԼ</button>
 
         <div class="switch-link">
-          <span>Չունե՞ք հաշիվ:</span> 
-          <a href="Reg.php">Գրանցվել <br> </a> 
+          <span>ՈՒնե՝ք հաշիվ:</span> 
+          <a href="login.php"> <br>Մուտք </a> 
                     <a href="index.php">Փակել</a>
 
         </div>
